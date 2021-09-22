ef701b8913abc6cb421b45eae50df1cd7c6463a9
Ali <aliknake@gmail.com>
2021-09-22 17:33:51 +0200

    added the back music sucessfully

--- src/components/myHeader.js
+++ src/components/myHeader.js
@@ -1,16 +1,25 @@
 import { useState } from 'react';
 import photo from '../media/ph.png'
+import ost from '../media/main_ost.mp3'
 
 const MyHeader =()=>{
-    const [name, setName] = useState('')
+    const [name, setName] = useState('');
+    const handleClick =()=>{
+        const backMusic = document.getElementById('backMusic');
+        backMusic.play();
+        setName('retract');
+    }
     return (
         <header className={name}>
             <img src={photo} alt="productive"/>
             <div> 
                 <h1>Productive</h1>
                 <p>your work managment area</p>
-                {name? null: <span onClick={()=>setName('retract')} > Let's Goooo </span>}
+                {name? null: <span onClick={handleClick} > Let's Go! </span>}
             </div>
+            <audio id="backMusic">
+                <source src={ost} type="audio/mpeg" />
+            </audio>
         </header>
     );
 }
