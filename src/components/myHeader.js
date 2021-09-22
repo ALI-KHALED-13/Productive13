4e1965ad8dc10c2c4213bf1b687d56cca4ca8d2a
Ali <aliknake@gmail.com>
2021-09-22 17:24:00 +0200

    done the intro animation

--- src/components/myHeader.js
+++ src/components/myHeader.js
@@ -1,13 +1,15 @@
+import { useState } from 'react';
 import photo from '../media/ph.png'
 
 const MyHeader =()=>{
-    
+    const [name, setName] = useState('')
     return (
-        <header>
+        <header className={name}>
             <img src={photo} alt="productive"/>
             <div> 
                 <h1>Productive</h1>
                 <p>your work managment area</p>
+                {name? null: <span onClick={()=>setName('retract')} > Let's Goooo </span>}
             </div>
         </header>
     );
