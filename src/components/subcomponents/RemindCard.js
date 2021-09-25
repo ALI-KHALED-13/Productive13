ff1389316616f9cda3716837773f635986fc119d
Ali <aliknake@gmail.com>
2021-09-25 15:57:22 +0200

    multible alrets bug fixed

--- src/components/subcomponents/RemindCard.js
+++ src/components/subcomponents/RemindCard.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const RemindCard =({reminder, reminders, updateReminders, playAlert})=>{
-    
+    const [fired, setFired] = useState(false);
+
     const handleDelete =(ev)=> {
         ev.target.parentElement.style.animation = 'fadeOutLeft 1s';
         setTimeout(()=> updateReminders(reminders.filter(obj=> obj !== reminder)), 600);
     }
+
     const timeTillAlert = new Date(reminder.date).getTime() - Date.now();
 
-    timeTillAlert > 0 && setTimeout(()=> {
-        playAlert().then(()=> setTimeout(()=> alert("it's time for " + reminder.title), 700));
-    } , timeTillAlert);
-
+    if (timeTillAlert > 0){
+        setTimeout(()=> {
+            playAlert();
+            setTimeout(()=>setFired(true), 700);
+        } , timeTillAlert);
+    }
+    if (fired) alert("it's time for " + reminder.title)
 
     return (
         <div className={"reminder" + (timeTillAlert > 0? '':" passed")} >
