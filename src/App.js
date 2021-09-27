07bae523fc929209472b6186e6948de9aa8b4894
Ali <aliknake@gmail.com>
2021-09-28 01:01:18 +0200

    useEffect to cleaTimeout on reminder removal

--- src/App.js
+++ src/App.js
@@ -6,12 +6,11 @@ import AliFooter from './components/AliFooter'
 
 
 function App(){
-    
     if (!localStorage.getItem("data")){
         localStorage.setItem('data', JSON.stringify({notes: [], lists: [], reminders: []}));
     }
     const data = JSON.parse(localStorage.getItem('data'));
-    
+
     return (
         <React.Fragment>
             <MyHeader />
--- src/components/subcomponents/RemindCard.js
+++ src/components/subcomponents/RemindCard.js
@@ -1,20 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const RemindCard =({reminder, reminders, updateReminders, playAlert})=>{
     const [fired, setFired] = useState(false);
-
-    const handleDelete =(ev)=> {
-        ev.target.parentElement.style.animation = 'fadeOutLeft 1s';
-        setTimeout(()=> updateReminders(reminders.filter(obj=> obj !== reminder)), 600);
-        clearTimeout(timerId);
-    }
-
     const timeTillAlert = new Date(reminder.date).getTime() - Date.now();
 
-    const timerId = setTimeout(fireAlert, timeTillAlert);
-    if (timeTillAlert <= 0) {
-        clearTimeout(timerId);
-        if(!fired) setFired(true);
+    useEffect(()=>{
+        const timerId = setTimeout(fireAlert, timeTillAlert);
+
+        if (timeTillAlert <= 0) {
+            clearTimeout(timerId);
+            if(!fired) setFired(true);
+        }
+        return ()=> clearTimeout(timerId);
+        // eslint-disable-next-line
+    }, [])
+    
+    const handleDelete =(ev)=> {
+        ev.target.parentElement.style.animation = 'fadeOutLeft 1s';
+        setTimeout(()=> updateReminders(reminders.filter(obj=> obj !== reminder)), 600);     
     }
 
     function fireAlert(){
